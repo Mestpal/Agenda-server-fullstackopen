82b09a0d966033e72584b377e8bb910233e6c62e
Manuel Estevez Palencia <manuestpal@gmail.com>
2025-08-13 17:48:56 +0200

    Exercise 3.6 Agenda 6

--- index.js
+++ index.js
@@ -54,5 +54,29 @@ app.delete('/api/persons/:id', (request, response) => {
   return response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({error: 'Person needs fields name and number to be valid'})
+  } else {
+    const found = persons.find( person => person.name.toLocaleLowerCase() === body.name.toLocaleLowerCase())
+
+    if (found) {
+      response.status(400).json({ error: 'name must be unique' })
+    } else {
+      const newPerson = {
+        id: Math.floor(Math.random() * 100000),
+        name: body.name,
+        number: body.number
+      }
+  
+      persons = persons.concat(newPerson)
+  
+      response.json(newPerson)
+    }
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server on port ${PORT}`))
