7102302b1abdd7a703eb465b8da748286d2b0a50
Manuel Estevez Palencia <manuestpal@gmail.com>
2025-08-13 17:10:09 +0200

    Exercise 3.4 Agenda 4

--- index.js
+++ index.js
@@ -4,46 +4,54 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/api/persons', (request, response) => {
-    return response.json(persons)
+  response.json(persons)
 })
 
 app.get('/info', (request, response) => {
-    return response.send(`<p> Phonebook has info for ${persons.length} people </p><p> ${new Date} </p>`)
+  response.send(`<p> Phonebook has info for ${persons.length} people </p><p> ${new Date} </p>`)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const Id = Number(request.params.id)
+  const Id = Number(request.params.id)
 
-    const person = persons.find(person => person.id === Id)
+  const person = persons.find(person => person.id === Id)
 
-    if (person) {
-      return response.json(person)
-    } else {
-      return response.status(404).send(`Person with id ${Id} does not exits`)
-    }
+  if (person) {
+    response.json(person)
+  } else {
+    return response.status(404).send(`Person with id ${Id} does not exits`)
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const Id = Number(request.params.id)
+
+  persons = persons.filter(person => person.id !== Id)
+
+  return response.status(204).end()
 })
 
 const PORT = 3001
